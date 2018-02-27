48146ff991025152b7a8f760e43ff26de93b59de
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-02-27 19:14:02 -0300

    testing ajax in rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,15 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( alert.js 
+                                                addresses.js
+                                                contacts.js
+                                                home.js
+                                                kinds.js
+                                                phones.js
+                                                addresses.css
+                                                contacts.css
+                                                home.css
+                                                kinds.css
+                                                phones.css)
+
