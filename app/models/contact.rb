e5e13fb6303a0ac6684230f68fe3d4264d56f080
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-02-28 23:02:37 -0300

    Fix error in model contact to the delete item

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,8 +4,8 @@ class Contact < ApplicationRecord
   validates_presence_of :kind
 
   belongs_to :kind #, optional: true
-  has_one :address
-  has_many :phones
+  has_one :address, dependent: :destroy
+  has_many :phones, dependent: :destroy
 
   accepts_nested_attributes_for :address, update_only: true
   accepts_nested_attributes_for :phones, reject_if: :all_blank, allow_destroy: true
